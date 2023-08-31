51cab4cd611991623fea7dd1e9d90717c85141c6
Dariusz Rybicki <dariusz@darrarski.pl>
2023-08-31 14:41:55 +0200

    Implement ContactReducer

--- app/Sources/ContactFeature/ContactReducer.swift
+++ app/Sources/ContactFeature/ContactReducer.swift
@@ -1,16 +1,73 @@
 import ComposableArchitecture
+import Foundation
 
-public struct ContactReducer: Reducer {
+public struct ContactReducer: Reducer, Sendable {
   public struct State: Equatable {
-    public init() {}
+    public init(
+      gravatar: GravatarJSON? = nil,
+      isLoading: Bool = false
+    ) {
+      self.gravatar = gravatar
+      self.isLoading = isLoading
+    }
+
+    var gravatar: GravatarJSON?
+    var isLoading: Bool
   }
 
-  public enum Action: Equatable {
+  public enum Action: Equatable, Sendable {
+    case fetchGravatar
+    case fetchGravatarResult(TaskResult<GravatarJSON>)
+    case view(View)
+
+    public enum View: Equatable, Sendable {
+      case refreshButtonTapped
+      case refreshTask
+      case task
+    }
   }
 
   public init() {}
 
+  @Dependency(\.gravatar) var gravatar
+
   public var body: some ReducerOf<Self> {
-    EmptyReducer()
+    Reduce { state, action in
+      enum CancelId { case fetchGravatar }
+
+      switch action {
+      case .fetchGravatar:
+        state.isLoading = true
+        return .run { send in
+          let result = await TaskResult {
+            try await gravatar.fetch(
+              email: "dariusz@darrarski.pl"
+            )
+          }
+          await send(.fetchGravatarResult(result))
+        }
+        .cancellable(id: CancelId.fetchGravatar, cancelInFlight: true)
+
+      case .fetchGravatarResult(let result):
+        state.isLoading = false
+        switch result {
+        case .success(let json):
+          state.gravatar = json
+
+        case .failure(_):
+          break
+        }
+        return .none
+
+      case .view(.refreshButtonTapped):
+        return .send(.fetchGravatar)
+
+      case .view(.refreshTask):
+        return .send(.fetchGravatar)
+
+      case .view(.task):
+        return .send(.fetchGravatar)
+      }
+    }
   }
 }
--- app/Tests/ContactFeatureTests/ContactReducerTests.swift
+++ app/Tests/ContactFeatureTests/ContactReducerTests.swift
@@ -4,11 +4,80 @@ import XCTest
 
 @MainActor
 final class ContactReducerTests: XCTestCase {
-  func testTask() async {
+  func testFetchGravatar() async {
+    let didFetchEmails = ActorIsolated<[String]>([])
+
     let store = TestStore(initialState: ContactReducer.State()) {
       ContactReducer()
+    } withDependencies: {
+      $0.gravatar.fetch = { @Sendable email in
+        await didFetchEmails.withValue { $0.append(email) }
+        return .preview
+      }
     }
 
-    await store.finish()
+    await store.send(.fetchGravatar) {
+      $0.isLoading = true
+    }
+    await didFetchEmails.withValue {
+      XCTAssertNoDifference($0, ["dariusz@darrarski.pl"])
+    }
+    await store.receive(.fetchGravatarResult(.success(.preview))) {
+      $0.isLoading = false
+      $0.gravatar = .preview
+    }
+  }
+
+  func testFetchGravatarFailure() async {
+    let error = NSError(domain: "", code: 0)
+    let store = TestStore(initialState: ContactReducer.State()) {
+      ContactReducer()
+    } withDependencies: {
+      $0.gravatar.fetch = { @Sendable _ in throw error }
+    }
+
+    await store.send(.fetchGravatar) {
+      $0.isLoading = true
+    }
+    await store.receive(.fetchGravatarResult(.failure(error))) {
+      $0.isLoading = false
+    }
+  }
+
+  func testViewTask() async {
+    let store = TestStore(initialState: ContactReducer.State()) {
+      ContactReducer()
+    } withDependencies: {
+      $0.gravatar.fetch = { @Sendable _ in .preview }
+    }
+    store.exhaustivity = .off
+
+    await store.send(.view(.task))
+    await store.receive(.fetchGravatar)
+  }
+
+  func testViewRefreshTask() async {
+    let store = TestStore(initialState: ContactReducer.State()) {
+      ContactReducer()
+    } withDependencies: {
+      $0.gravatar.fetch = { @Sendable _ in .preview }
+    }
+    store.exhaustivity = .off
+
+    await store.send(.view(.refreshTask))
+    await store.receive(.fetchGravatar)
+
+  }
+
+  func testViewRefreshButtonTapped() async {
+    let store = TestStore(initialState: ContactReducer.State()) {
+      ContactReducer()
+    } withDependencies: {
+      $0.gravatar.fetch = { @Sendable _ in .preview }
+    }
+    store.exhaustivity = .off
+
+    await store.send(.view(.refreshButtonTapped))
+    await store.receive(.fetchGravatar)
   }
 }
