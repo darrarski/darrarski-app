9fc20f39994c964f02271ab9fba233773d07c5f4
Dariusz Rybicki <dariusz@darrarski.pl>
2023-09-01 17:03:15 +0200

    Refactor

--- app/Sources/Mastodon/Utilities/JSONDecoder.swift
+++ app/Sources/Mastodon/Utilities/JSONDecoder.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension JSONDecoder {
-  static let mastodon: JSONDecoder = {
+  public static let mastodon: JSONDecoder = {
     let decoder = JSONDecoder()
     decoder.keyDecodingStrategy = .convertFromSnakeCase
     decoder.dateDecodingStrategy = .custom { decoder in
