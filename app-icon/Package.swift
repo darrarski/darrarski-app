900ccdcaac1359c9d75c6f12e4868024ec8bbd3c
Dariusz Rybicki <dariusz@darrarski.pl>
2023-09-08 10:58:57 +0200

    Add app-icon package

---
+++ app-icon/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+  name: "app-icon",
+  platforms: [
+    .macOS(.v13),
+  ],
+  products: [
+    .executable(name: "app-icon-export", targets: ["app-icon-export"]),
+    .library(name: "AppIcon", targets: ["AppIcon"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/darrarski/swiftui-app-icon-creator.git", from: "1.2.0"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "app-icon-export",
+      dependencies: [
+        .target(name: "AppIcon"),
+      ]
+    ),
+    .target(
+      name: "AppIcon",
+      dependencies: [
+        .product(name: "AppIconCreator", package: "swiftui-app-icon-creator"),
+      ]
+    ),
+  ]
+)
