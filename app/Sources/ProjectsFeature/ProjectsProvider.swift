d9d009f74ccd896239ae1c3fbee7c802dad3fb3e
Dariusz Rybicki <dariusz@darrarski.pl>
2023-09-08 16:23:17 +0200

    Add ProjectsProvider.FetchInfo

--- app/Sources/ProjectsFeature/ProjectsProvider.swift
+++ app/Sources/ProjectsFeature/ProjectsProvider.swift
@@ -3,12 +3,18 @@ import Foundation
 import XCTestDynamicOverlay
 
 public struct ProjectsProvider: Sendable {
+  public typealias FetchInfo = @Sendable () async throws -> ProjectsInfo
   public typealias FetchProjects = @Sendable () async throws -> [Project]
 
-  public init(fetchProjects: @escaping FetchProjects) {
+  public init(
+    fetchInfo: @escaping FetchInfo,
+    fetchProjects: @escaping FetchProjects
+  ) {
+    self.fetchInfo = fetchInfo
     self.fetchProjects = fetchProjects
   }
 
+  public var fetchInfo: FetchInfo
   public var fetchProjects: FetchProjects
 }
 
@@ -21,10 +27,15 @@ extension DependencyValues {
 
 extension ProjectsProvider: TestDependencyKey {
   public static let testValue = ProjectsProvider(
+    fetchInfo: unimplemented("\(Self.self).fetchInfo"),
     fetchProjects: unimplemented("\(Self.self).fetchProjects")
   )
 
   public static let previewValue = ProjectsProvider(
+    fetchInfo: {
+      try await Task.sleep(for: .seconds(0.5))
+      return .preview
+    },
     fetchProjects: {
       try await Task.sleep(for: .seconds(1))
       return .preview
@@ -47,7 +58,40 @@ extension ProjectsProvider: DependencyKey {
     public var data: Data
   }
 
+  public struct InvalidResponseError: Error {
+    public init(statusCode: Int, data: Data) {
+      self.statusCode = statusCode
+      self.data = data
+    }
+    
+    public var statusCode: Int
+    public var data: Data
+  }
+
   public static let liveValue = ProjectsProvider(
+    fetchInfo: {
+      @Dependency(\.urlSession) var urlSession
+
+      var urlComponents = URLComponents()
+      urlComponents.scheme = "https"
+      urlComponents.host = "app.darrarski.pl"
+      urlComponents.path = "/projects.md"
+      guard let url = urlComponents.url else {
+        throw InvalidURLError()
+      }
+      var urlRequest = URLRequest(url: url)
+      urlRequest.httpMethod = "GET"
+      let (data, response) = try await urlSession.data(for: urlRequest)
+      let statusCode = (response as? HTTPURLResponse)?.statusCode
+      guard let statusCode, (200..<300).contains(statusCode) else {
+        throw ResponseError(statusCode: statusCode, data: data)
+      }
+      guard let string = String(data: data, encoding: .utf8) else {
+        throw InvalidResponseError(statusCode: statusCode, data: data)
+      }
+
+      return ProjectsInfo(markdown: string)
+    },
     fetchProjects: {
       @Dependency(\.urlSession) var urlSession
 
