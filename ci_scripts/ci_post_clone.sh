9c7c9d562e7ef3300c09d7c29f359b8dc15f0f0d
Dariusz Rybicki <dariusz@darrarski.pl>
2023-09-08 14:02:20 +0200

    Add CI post-clone script
    When running "Deploy" workflow:
    1. Add git tag with marketing version and build number.
    2. Generate WhatToTest file from commits from previous tag.

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+realpath() {
+  local path=`eval echo "$1"`
+  local folder=$(dirname "$path")
+  echo $(cd "$folder"; pwd)/$(basename "$path"); 
+}
+
+if [[ $CI_WORKFLOW == "Deploy" ]]; then
+  cd "$(dirname $(realpath $0))"
+
+  PROJECT_PATH="../project/DarrarskiApp.xcodeproj/project.pbxproj"
+  MARKETING_VERSION=$(sed -n '/MARKETING_VERSION/{s/MARKETING_VERSION = //;s/;//;s/^[[:space:]]*//;p;q;}' ${PROJECT_PATH})
+  BUILD_TAG=v${MARKETING_VERSION}-${CI_BUILD_NUMBER}
+  TESTFLIGHT_DIR_PATH=../TestFlight
+  WHAT_TO_TEST_PATH=$TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+
+  # Add and push build tag:
+  echo "Adding git tag: ${BUILD_TAG}"
+  git tag -a -f -m "" ${BUILD_TAG}
+  git push https://darrarski:${GITHUB_PAT}@github.com/darrarski/darrarski-app.git ${BUILD_TAG}
+
+  # Generate WhatToTest file from commits since previous tag:
+  mkdir $TESTFLIGHT_DIR_PATH
+  git fetch --unshallow --tags
+  git log \
+    $(git describe --tags --abbrev=0 HEAD~1)..HEAD \
+    --pretty=format:'- [%h] %s' \
+    --first-parent \
+    > $WHAT_TO_TEST_PATH
+  echo "\n- More info: https://github.com/darrarski/darrarski-app/releases/tag/${BUILD_TAG}" \
+    >> $WHAT_TO_TEST_PATH
+  echo "What To Test:"
+  cat $WHAT_TO_TEST_PATH
+fi
