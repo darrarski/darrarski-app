4abba6a0d402d4eae891997675b1ea47feb5b04b
Dariusz Rybicki <dariusz@darrarski.pl>
2023-09-10 01:59:58 +0200

    Send App.init signals

--- app/Sources/AppFeature/App.swift
+++ app/Sources/AppFeature/App.swift
@@ -3,6 +3,12 @@ import SwiftUI
 
 @main
 struct App: SwiftUI.App {
+  init() {
+    appTelemetry.initialize()
+    appTelemetry.send("App.init")
+  }
+
+  @Dependency(\.appTelemetry) var appTelemetry
   let store = Store(initialState: AppReducer.State()) {
     AppReducer()
   }
