45c51535321440b728a318a8aa52652aaeb4b6d6
Dariusz Rybicki <dariusz@darrarski.pl>
2023-09-10 15:27:18 +0200

    Enhance AppTelemetryReducerTests

--- app/Tests/AppFeatureTests/AppTelemetryReducerTests.swift
+++ app/Tests/AppFeatureTests/AppTelemetryReducerTests.swift
@@ -5,55 +5,106 @@ import XCTest
 @MainActor
 final class AppTelemetryReducerTests: XCTestCase {
   func testActionSignals() async {
+    struct ExampleReducer: Reducer {
+      struct State: Equatable {}
+
+      enum Action: Equatable {
+        case first
+        case second(String)
+        case third(Int, String)
+        case fourth(number: Int, text: String)
+        case fifth(String?)
+        case sixth(SixthAction)
+
+        enum SixthAction: Equatable {
+          case seventh
+        }
+      }
+      
+      var body: some ReducerOf<Self> {
+        EmptyReducer()
+      }
+    }
     let signals = LockIsolated<[AppTelemetrySignal]>([])
-    let store = TestStore(initialState: AppReducer.State()) {
-      AppTelemetryReducerOf<AppReducer>()
+    let store = TestStore(initialState: ExampleReducer.State()) {
+      ExampleReducer()
+      AppTelemetryReducer()
     } withDependencies: {
       $0.appTelemetry.send = { @Sendable signal in
         signals.withValue { $0.append(signal) }
       }
     }
 
-    await store.send(.view(.sectionSelected(.contact)))
+    await store.send(.first)
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.view(.sectionSelected(.some(.contact)))"
+      type: "\(Self.self).ExampleReducer.Action.first"
     )])
     signals.setValue([])
 
-    await store.send(.view(.sectionSelected(.feed)))
+    await store.send(.second("test"))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.view(.sectionSelected(.some(.feed)))"
+      type: "\(Self.self).ExampleReducer.Action.second(String)"
     )])
     signals.setValue([])
 
-    await store.send(.view(.sectionSelected(.none)))
+    await store.send(.third(7, "seven"))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.view(.sectionSelected(.none))"
+      type: "\(Self.self).ExampleReducer.Action.third(Int, String)"
     )])
     signals.setValue([])
 
-    await store.send(.contact(.view(.task)))
+    await store.send(.fourth(number: 7, text: "seven"))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.contact(.view(.task))"
+      type: "\(Self.self).ExampleReducer.Action.fourth(number: Int, text: String)"
     )])
     signals.setValue([])
 
-    await store.send(.contact(.fetchContact))
+    await store.send(.fifth("test"))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.contact(.fetchContact)"
+      type: "\(Self.self).ExampleReducer.Action.fifth(.some(String))"
     )])
     signals.setValue([])
 
-    await store.send(.contact(.fetchContactResult(.success(.preview))))
+    await store.send(.fifth(nil))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.contact(.fetchContactResult(.success(Contact)))"
+      type: "\(Self.self).ExampleReducer.Action.fifth(.none)"
     )])
     signals.setValue([])
+
+    await store.send(.sixth(.seventh))
+    XCTAssertNoDifference(signals.value, [.init(
+      type: "\(Self.self).ExampleReducer.Action.sixth(.seventh)"
+    )])
+    signals.setValue([])
+  }
+
+  func testFailureActionSignals() async {
+    struct ExampleReducer: Reducer {
+      struct State: Equatable {}
+
+      enum Action: Equatable {
+        case first(TaskResult<String>)
+        case second(NSError, NSError, NSError)
+      }
+
+      var body: some ReducerOf<Self> {
+        EmptyReducer()
+      }
+    }
+    let signals = LockIsolated<[AppTelemetrySignal]>([])
+    let store = TestStore(initialState: ExampleReducer.State()) {
+      ExampleReducer()
+      AppTelemetryReducer()
+    } withDependencies: {
+      $0.appTelemetry.send = { @Sendable signal in
+        signals.withValue { $0.append(signal) }
+      }
+    }
 
     let nsError = NSError(domain: "test", code: 1337)
-    await store.send(.contact(.fetchContactResult(.failure(nsError))))
+    await store.send(.first(.failure(nsError)))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.contact(.fetchContactResult(.failure(NSError)))",
+      type: "\(Self.self).ExampleReducer.Action.first(.failure(NSError))",
       payload: [
         "error.localizedDescription": nsError.localizedDescription,
         "error.domain": nsError.domain,
@@ -64,9 +115,9 @@ final class AppTelemetryReducerTests: XCTestCase {
 
     struct StructError: Error { var value = 44 }
     let structError = StructError()
-    await store.send(.contact(.fetchContactResult(.failure(structError))))
+    await store.send(.first(.failure(structError)))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.contact(.fetchContactResult(.failure(\(Self.self).StructError)))",
+      type: "\(Self.self).ExampleReducer.Action.first(.failure(\(Self.self).StructError))",
       payload: [
         "error.localizedDescription": structError.localizedDescription,
         "error.domain": (structError as NSError).domain,
@@ -75,11 +126,11 @@ final class AppTelemetryReducerTests: XCTestCase {
     )])
     signals.setValue([])
 
-    enum EnumError: Error { case failure }
-    let enumError = EnumError.failure
-    await store.send(.contact(.fetchContactResult(.failure(enumError))))
+    enum EnumError: Error { case somethingWentWrong }
+    let enumError = EnumError.somethingWentWrong
+    await store.send(.first(.failure(enumError)))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.contact(.fetchContactResult(.failure(\(Self.self).EnumError.failure)))",
+      type: "\(Self.self).ExampleReducer.Action.first(.failure(\(Self.self).EnumError.somethingWentWrong))",
       payload: [
         "error.localizedDescription": enumError.localizedDescription,
         "error.domain": (enumError as NSError).domain,
@@ -88,9 +139,23 @@ final class AppTelemetryReducerTests: XCTestCase {
     )])
     signals.setValue([])
 
-    await store.send(.feed(.status(id: "test-id", action: .view(.linkTapped(URL(fileURLWithPath: "test.url"))))))
+    let nsError1 = NSError(domain: "test1", code: 1)
+    let nsError2 = NSError(domain: "test2", code: 2)
+    let nsError3 = NSError(domain: "test3", code: 3)
+    await store.send(.second(nsError1, nsError2, nsError3))
     XCTAssertNoDifference(signals.value, [.init(
-      type: "AppReducer.Action.feed(.status(id: String, action: .view(.linkTapped(URL))))"
+      type: "\(Self.self).ExampleReducer.Action.second(NSError, NSError, NSError)",
+      payload: [
+        "error.localizedDescription": nsError1.localizedDescription,
+        "error.domain": nsError1.domain,
+        "error.code": "\(nsError1.code)",
+        "error.localizedDescription_1": nsError2.localizedDescription,
+        "error.domain_1": nsError2.domain,
+        "error.code_1": "\(nsError2.code)",
+        "error.localizedDescription_2": nsError3.localizedDescription,
+        "error.domain_2": nsError3.domain,
+        "error.code_2": "\(nsError3.code)",
+      ]
     )])
     signals.setValue([])
   }
