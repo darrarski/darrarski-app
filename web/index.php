d32321b90149d6aa0245b812b01509f77cb3d76b
Dariusz Rybicki <dariusz@darrarski.pl>
2023-09-12 15:23:21 +0200

    Redirect web to GitHub repo

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER['REQUEST_URI'] == '/') {
   http_response_code(307);
-  header('Location: https://darrarski.pl');
+  header('Location: https://github.com/darrarski/darrarski-app');
 } else {
   http_response_code(404);
   echo("not found");
