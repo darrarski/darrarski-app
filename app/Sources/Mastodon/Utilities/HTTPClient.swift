c81ecae492320f359ac4f6db3fe07fa4d4dd47f5
Dariusz Rybicki <dariusz@darrarski.pl>
2024-01-29 13:06:43 +0100

    Refactor Mastodon API client

---
+++ app/Sources/Mastodon/Utilities/HTTPClient.swift
@@ -0,0 +1,29 @@
+import Dependencies
+import DependenciesMacros
+import Foundation
+
+public struct HTTPClient: Sendable {
+  @DependencyEndpoint
+  public var dataForRequest: @Sendable (URLRequest) async throws -> (Data, URLResponse)
+
+  public func data(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+    try await dataForRequest(urlRequest)
+  }
+}
+
+extension HTTPClient: DependencyKey {
+  public static let testValue = HTTPClient()
+  public static var liveValue = HTTPClient(
+    dataForRequest: { request in
+      @Dependency(\.urlSession) var urlSession
+      return try await urlSession.data(for: request)
+    }
+  )
+}
+
+extension DependencyValues {
+  public var httpClient: HTTPClient {
+    get { self[HTTPClient.self] }
+    set { self[HTTPClient.self] = newValue }
+  }
+}
