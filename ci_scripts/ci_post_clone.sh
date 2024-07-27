c89f9e1e0c3adb6c23411a8092e1c2a149a90603
Dariusz Rybicki <dariusz@darrarski.pl>
2024-07-27 03:39:59 +0200

    Update scripts

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,8 +4,12 @@ source "$(readlink -f $0 | xargs dirname | xargs dirname)/scripts/_shared.sh"
 # Skip validation of third-party swift macros
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
-# Show rust's backtrace on errors
-export RUST_BACKTRACE=1
+# Setup dev-tools
+export COLORBT_SHOW_HIDDEN=1
+export RUST_BACKTRACE=full
+setup_mise
+mise install tuist || exit $?
+echo "Tuist v$(mise x -- tuist version) ($(mise which tuist))"
 
 # Setup Xcode workspace
 "$ROOT_DIR/scripts/setup_for_development.sh" || exit $?
