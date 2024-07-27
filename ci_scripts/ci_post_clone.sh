f4488a23140948dcd6cd1f8e5664a5f84e892632
Dariusz Rybicki <dariusz@darrarski.pl>
2024-07-27 03:19:27 +0200

    Update scripts

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,8 +4,8 @@ source "$(readlink -f $0 | xargs dirname | xargs dirname)/scripts/_shared.sh"
 # Skip validation of third-party swift macros
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
-# Install mise locally
-export MISE_INSTALL_PATH="$ROOT_DIR/.local/bin/mise"
+# Install mise
+"$ROOT_DIR/scripts/install_mise.sh" --binary || exit $?
 
 # Setup Xcode workspace
 "$ROOT_DIR/scripts/setup_for_development.sh" || exit $?
