3f0f164380604f9b4081b1e4aee6c0f7a27cac28
Dariusz Rybicki <dariusz@darrarski.pl>
2024-07-30 03:22:42 +0200

    Enable feature flags in app-secrets package

--- Projects/App/app-secrets/Package.swift
+++ Projects/App/app-secrets/Package.swift
@@ -19,3 +19,48 @@ let package = Package(
     ),
   ]
 )
+
+for target in package.targets where target.type == .regular {
+  var swiftSettings = target.swiftSettings ?? []
+  swiftSettings.append(.unsafeFlags([
+    "-Xfrontend", "-enable-actor-data-race-checks",
+    "-Xfrontend", "-debug-time-function-bodies",
+    "-Xfrontend", "-debug-time-expression-type-checking"
+  ], .when(configuration: .debug)))
+#if !hasFeature(ConciseMagicFile)
+  swiftSettings.append(.enableUpcomingFeature("ConciseMagicFile"))
+#endif
+#if !hasFeature(ForwardTrailingClosures)
+  swiftSettings.append(.enableUpcomingFeature("ForwardTrailingClosures"))
+#endif
+#if !hasFeature(StrictConcurrency)
+  swiftSettings.append(.enableUpcomingFeature("StrictConcurrency"))
+  swiftSettings.append(.enableExperimentalFeature("StrictConcurrency"))
+#endif
+#if !hasFeature(BareSlashRegexLiterals)
+  swiftSettings.append(.enableUpcomingFeature("BareSlashRegexLiterals"))
+#endif
+#if !hasFeature(ImplicitOpenExistentials)
+  swiftSettings.append(.enableUpcomingFeature("ImplicitOpenExistentials"))
+#endif
+#if !hasFeature(ImportObjcForwardDeclarations)
+  swiftSettings.append(.enableUpcomingFeature("ImportObjcForwardDeclarations"))
+#endif
+#if !hasFeature(DisableOutwardActorInference)
+  swiftSettings.append(.enableUpcomingFeature("DisableOutwardActorInference"))
+#endif
+#if !hasFeature(InternalImportsByDefault)
+  swiftSettings.append(.enableUpcomingFeature("InternalImportsByDefault"))
+#endif
+#if !hasFeature(IsolatedDefaultValues)
+  swiftSettings.append(.enableUpcomingFeature("IsolatedDefaultValues"))
+#endif
+#if !hasFeature(GlobalConcurrency)
+  swiftSettings.append(.enableUpcomingFeature("GlobalConcurrency"))
+#endif
+#if !hasFeature(ExistentialAny)
+  swiftSettings.append(.enableUpcomingFeature("ExistentialAny"))
+#endif
+  target.swiftSettings = swiftSettings
+}
+
