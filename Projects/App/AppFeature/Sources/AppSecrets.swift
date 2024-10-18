d089af5735f35cd8e68d478038f980edb4c32bff
Dariusz Rybicki <dariusz@darrarski.pl>
2024-10-18 13:46:27 +0200

    Fix swift warnings

--- Projects/App/AppFeature/Sources/AppSecrets.swift
+++ Projects/App/AppFeature/Sources/AppSecrets.swift
@@ -2,7 +2,7 @@ import AppSecrets
 import Dependencies
 import Foundation
 
-extension AppSecrets: DependencyKey {
+extension AppSecrets: Dependencies.DependencyKey {
   public static let liveValue = AppSecrets.live
   public static let testValue = AppSecrets()
   public static let previewValue = AppSecrets()
